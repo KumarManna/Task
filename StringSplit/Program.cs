face5da0f73f4a95e1a998ef48f21626121071ea
abhijitece <101515727+abhijitece@users.noreply.github.com>
2022-07-31 21:46:51 +0530

    Add project files.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StringSplit
+{
+    class StringDivide:SubString
+    {
+        static void Main(string[] args)
+        {
+            StringDivide sd = new StringDivide();
+            string m = Console.ReadLine();
+            List<char> k= sd.Sub(m);
+            foreach(char x in k)
+            {
+                Console.Write(x);
+            }
+    
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringSplit/SubString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplit
+{
+    class SubString
+    { 
+        protected List<char> Sub(string k)
+        {
+            //string k = "Apple,Banana,Cherry,Mango,This is test.";
+            //string k = Console.ReadLine();
+            char[] c = k.ToCharArray();
+            List<char> ch = new List<char>();
+            //char[] seperators = { '.', ';', ',', '!', '&' };
+
+            foreach (char m in c)
+            {
+                if (m == ' ' || m == '.' || m == ',' || m == ';')
+                {
+                    ch.Add('\n');
+                }
+              
+                else
+                    ch.Add(m);
+
+            }/*
+            foreach(char x in ch)
+            {
+
+                Console.Write(x.ToString());
+            }*/
+            return ch;
+        }
+    }
+}
